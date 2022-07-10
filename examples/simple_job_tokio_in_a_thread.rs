859e7192a0940c037c8a8afb998dccd635668f7b
Michael van Niekerk <mike@agri-io.co.za>
2022-07-10 08:33:57 +0200

    Add threaded own runtime setup on tokio example

---
+++ examples/simple_job_tokio_in_a_thread.rs
@@ -0,0 +1,37 @@
+use crate::lib::run_example;
+use std::error::Error;
+use std::time::Duration;
+use tokio_cron_scheduler::{Job, JobScheduler};
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+mod lib;
+use tokio_cron_scheduler::JobToRun;
+
+fn main() {
+    let handle = std::thread::Builder::new()
+        .name("schedule thread".to_string())
+        .spawn(move || {
+            // tokio::runtime::Builder::new_current_thread()    <- This hangs
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .expect("build runtime failed")
+                .block_on(start())
+                .expect("TODO: panic message");
+        })
+        .expect("spawn thread failed");
+    handle.join().expect("join failed");
+}
+
+async fn start() -> Result<(), Box<dyn Error>> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("Setting default subscriber failed");
+    info!("Creating scheduler");
+    let sched = JobScheduler::new()?;
+    info!("Run example");
+    run_example(sched).await;
+    Ok(())
+}
